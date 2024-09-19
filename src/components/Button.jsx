66dd6f1d2237c730b92c703a064d4cdc071c88a7
Matheus Marchetti <matheusmarchetti1502@gmail.com>
2024-09-19 19:19:54 -0300

    padronização código e botão

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const Button = ({ onPress, title }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#1E90FF",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
+
+export default Button;
